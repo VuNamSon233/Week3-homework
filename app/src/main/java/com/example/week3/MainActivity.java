8e34ba76382e30ffa30f15fb24ee647e88056c24
SƠN VŨ NAM <21200203@student.hcmus.edu.vn>
2025-03-24 13:17:45 +0700

    Initial commit for Week3

---
+++ app/src/main/java/com/example/week3/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.week3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText student_type;
+    TextView teacher_fix;
+    TextView result;
+    Button btn_send;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // Find view by ID
+        student_type = findViewById(R.id.studentWrite_Text);
+        btn_send = findViewById(R.id.buttonAssign);
+        result = findViewById(R.id.teacherFix_received);
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String student_str = student_type.getText().toString();
+                result.setText(student_str);
+            }
+        });
+
+    }
+}
